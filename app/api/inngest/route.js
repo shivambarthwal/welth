20ebcfa6e5eb49c5ec9a3e860b717d3e4c0b813f
Shivam barthwal <shivam.paulbarthwal@gmail.com>
2025-06-15 16:06:28 +0530

    updated

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,13 +1,10 @@
-import { serve } from "inngest/next";
-import { inngest } from "@/lib/inngest/client";
-import {
-  checkBudgetAlerts,
-  generateMonthlyReports,
-  processRecurringTransaction,
-  triggerRecurringTransactions,
-} from "@/lib/inngest/function";
 
-const handlers = serve({
+import { serve } from "inngest/next";
+
+import { checkBudgetAlerts, generateMonthlyReports, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/function";
+import { inngest } from "@/lib/inngest/init";
+
+export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     processRecurringTransaction,
@@ -16,7 +13,3 @@ const handlers = serve({
     checkBudgetAlerts,
   ],
 });
-
-export const GET = handlers.GET;
-export const POST = handlers.POST;
-export const PUT = handlers.PUT;
